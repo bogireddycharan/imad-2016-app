2b45933ffbcf403bb1272e8b5bd1d1e53b79343f
bogireddycharan <bogireddycharan@gmail.com>
2016-09-23 21:24:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-one',function (req, res)
-{res.sendFile('Article one requested and ....');
+app.get('/article-one',function (req, res){
+    res.sendFile('Article one requested and ....');
     
 });
 
