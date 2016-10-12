1d516f0a5662e6a4868c3eef34bb48391707975e
bogireddycharan <bogireddycharan@gmail.com>
2016-10-12 12:38:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) {.
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/', function (req, res) {
@@ -20,6 +20,10 @@ var articleone= {
     title:"Article one gurucharan",
     heading:"guru article",
     date:"sept 25",
+    content: 
+    
+   " <p> hi this is para under content</p>",
+    
     
     
 };
